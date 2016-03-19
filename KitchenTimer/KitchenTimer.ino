193e8362e0d95228cd19b31d441cacb8a0815833
Wakabyashi, Kenichi <kwaka1208@pote2.net>
2016-03-19 14:38:09 +0900

    一旦完成
    もうちょっとシンプルにできないかなと思うけど、とりあえず所定の動作になったので、コミット。

--- KitchenTimer/KitchenTimer.ino
+++ KitchenTimer/KitchenTimer.ino
@@ -1,22 +1,23 @@
 enum STATE {
-  STATE_WAIT,   // Wait for start
-  STATE_COUNT,    // Counting down
-  STATE_COMPLETE  // Timer 
+	STATE_WAIT,   // Wait for start
+	STATE_COUNT,  // Counting down
+	STATE_COMPLETE  // Timer 
 };
 
-volatile int currentState = STATE_WAIT;     // Status
-volatile int count = 0;         // Timer Value 1 = 1min
-volatile unsigned long startTime = 0; // Start Time
+#define UNIT_TIME 60
+volatile int currentState = STATE_WAIT;			// Status
+volatile unsigned long countSec = 0;				// Timer Value 1 = 1sec
+volatile unsigned long startTime = 0;			// Start Time
+volatile unsigned long lastInteruptTime = 0;	// The time that last interupt occured.
 
 void setup(){
-	pinMode(13, OUTPUT);		// LED
-	pinMode(2, INPUT_PULLUP);	// Switch
-	attachInterrupt(0, SwitchPressed, FALLING);
-  Serial.begin(9600);
+	pinMode(13, OUTPUT);    // LED
+	pinMode(2, INPUT_PULLUP); // Switch
+	attachInterrupt(0, OnSwitchPress, FALLING);
+	Serial.begin(9600);
 }
 
-#define UNIT_TIME (60*1000)
-
+//#define UNIT_TIME (60*1000) //
 
 void loop() {
 	switch(currentState) {
@@ -29,22 +30,20 @@ void loop() {
 		break;
 
 	case STATE_WAIT:
-	default:
+		default:
 		break;
 	}
 }
 /** 
-	Judge Timeout
+Judge Timeout
 */
 bool IsTimeOut(int count, unsigned long start) {
 	bool ret = false;
-	unsigned long now;
-	long elapsed = (long)(millis() - startTime);
-	Serial.print(startTime, DEC);
-	Serial.print(elapsed, DEC);
-
-
-	if ((long)(elapsed / UNIT_TIME) > (long)count) {
+	if (currentState != STATE_COUNT) return ret;
+	unsigned long elapsedSec = (millis() - startTime) / 1000;
+	Serial.print("elapsedSec is ");
+	Serial.println(elapsedSec, DEC);
+	if (elapsedSec >= count) {
 		ret = true;
 		currentState = STATE_COMPLETE;
 	}
@@ -52,10 +51,10 @@ bool IsTimeOut(int count, unsigned long start) {
 }
 
 /**
- * Blink LED
+* Blink LED
 */
 void Blink(int onInverval, int offInverval) {
-	if (IsTimeOut(count, startTime)) return;
+	if (IsTimeOut(countSec, startTime)) return;
 	// Turn on the LED
 	if (onInverval != 0) {
 		digitalWrite(13, HIGH);
@@ -64,36 +63,57 @@ void Blink(int onInverval, int offInverval) {
 
 	if (offInverval != 0) {
 		digitalWrite(13, LOW);
-		if (IsTimeOut(count, startTime)) return;
+		if (IsTimeOut(countSec, startTime)) return;
 		// Turn off the LED
 		delay(offInverval);
 	}
 }
 
 /**
- * Switch Interupt
+* Debug information output
 */
-void SwitchPressed() {
-  Serial.println("PRESSED");
+void debugInfo() {
+	Serial.print("Count is ");
+	Serial.println(countSec, DEC);
+	Serial.print("Start Time is ");
+	Serial.println(startTime, DEC);
+	Serial.print("Current Time is ");
+	Serial.println(millis(), DEC);
+}
+
+/**
+* Switch Interupt
+*/
+void OnSwitchPress() {
+	if (millis() - lastInteruptTime < 500 ) {
+		return;
+	}
+	lastInteruptTime = millis();
+	Serial.println("PRESSED");
 	switch(currentState) {
 	case STATE_COUNT:
-		count++;
+		countSec += UNIT_TIME;
+		debugInfo();
 		break;
 
 	case STATE_COMPLETE:
 		currentState = STATE_WAIT;
 		// Turn off the LED
 		digitalWrite(13, LOW);
-		count = 0;
+		countSec = 0;
 		break;
 
 	case STATE_WAIT:
 		currentState = STATE_COUNT;
+		countSec = 1 * UNIT_TIME;
 		startTime = millis();
-		count++;
+		debugInfo();
 		break;
 
 	default:
 		break;
 	}
 }
+
+
+
